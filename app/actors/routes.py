59a75232bc53f48f53c1e569cc8cab356f51d854
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-12 11:15:30 +0100

    .

--- app/actors/routes.py
+++ app/actors/routes.py
@@ -2,7 +2,9 @@ from flask import Blueprint
 from utils.functions import *
 
 actorsBP = Blueprint("actors", __name__)
+filePath = "app/files/actors.json"
 
 @actorsBP.get("/")
 def getActors():
-    pass
+    global filePath
+    return readFile(filePath)
