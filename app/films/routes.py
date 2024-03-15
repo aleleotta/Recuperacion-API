356b166fbd9368a135e17147a977bba8bc52efb2
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-15 12:37:21 +0100

    .

--- app/films/routes.py
+++ app/films/routes.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 from utils.functions import *
 
 filmsBP = Blueprint("films", __name__)
+filePath = "app/files/films.json"
+
+@filmsBP.get("/")
+def getFilms():
+    global filePath
+    return readFile(filePath)
