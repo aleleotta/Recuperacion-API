bfcd846240403f7a910735573da51b8e7d64ac7d
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-16 17:18:48 +0100

    .

--- app/actors/routes.py
+++ app/actors/routes.py
@@ -12,8 +12,19 @@ def getActors():
 @actorsBP.get("/<int:id>")
 def getSpecificActor(id):
     global filePath
-    json = readFile(filePath)
-    for actor in json:
+    actors = readFile(filePath)
+    for actor in actors:
         if actor["id"] == id:
             return actor, 200
-    return {"error": "Actor not found."}, 404
+    return {"error": "The following actor was not found."}, 404
+
+def deleteActor(id):
+    global filePath
+    actors = readFile(filePath)
+    for actor in actors:
+        if actor["id"] == id:
+            actorName = actor["actor_name"]
+            actors.remove(actor)
+            writeFile(filePath, actors)
+            return {"message": f"The following actor was deleted: {actorName}"}, 200
+    return {"error": "The following actor was not found."}, 404
--- app/films/routes.py
+++ app/films/routes.py
@@ -2,18 +2,36 @@ from flask import Blueprint
 from utils.functions import *
 
 filmsBP = Blueprint("films", __name__)
-filePath = "app/files/films.json"
+filmsFilePath = "app/files/films.json"
+actorsFilePath = "app/files/actors.json"
 
 @filmsBP.get("/")
 def getFilms():
-    global filePath
-    return readFile(filePath), 200
+    global filmsFilePath
+    return readFile(filmsFilePath), 200
 
 @filmsBP.get("/<int:id>")
 def getSpecificFilm(id):
-    global filePath
-    json = readFile(filePath)
-    for film in json:
+    global filmsFilePath
+    films = readFile(filmsFilePath)
+    for film in films:
         if film["id"] == id:
             return film, 200
+    return {"error": "Film not found."}, 404
+
+# Will return the actors of a specific film
+@filmsBP.get("/<int:id>/actors")
+def getActorsFromFilm(filmId):
+    global filmsFilePath, actorsFilePath
+    films = readFile(filmsFilePath)
+    actors = readFile(actorsFilePath)
+    for film in films:
+        if film["id"] == filmId:
+            filmActors = []
+            for actor in actors:
+                if actor["id_movie"] == filmId:
+                    filmActors.append(actor)
+            if len(filmActors) == 0:
+                return {"error": "No actors found for this film."}, 404
+            return filmActors, 200
     return {"error": "Film not found."}, 404
