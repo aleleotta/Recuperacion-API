06856b86f9852d2f9598d3727fb6f30eb2ca1203
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-09 12:30:41 +0200

    .

--- tests/actors/get.py
+++ tests/actors/get.py
@@ -1,2 +1,12 @@
+import json
 import requests
-baseURL = "http://localhost:5050/actors/"
+baseURL = "http://localhost:5050/actors"
+response = requests.get(baseURL)
+actorsList = response.json()
+print("---------------------------------------------------------------------------------------------")
+print("\nList:")
+i = 1
+for actor in actorsList:
+    print(f"\t{i}) {actor['actor_name']}\n")
+    i = i + 1
+print("---------------------------------------------------------------------------------------------")
